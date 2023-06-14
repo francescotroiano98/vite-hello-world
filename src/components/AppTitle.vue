dc6ec0cc5c3f13f754957c8eee407b469389f6e2
francescotroiano98 <francesco-troiano-98@libero.it>
2023-06-14 17:19:46 +0200

    push pseudo soluction

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <div>
+      <h1>
+        {{ message }}
+      </h1>
+    </div>
+    
+  </template>
+  
+  <script>
+  
+    export default{
+      data(){
+  
+        return{
+          message: 'La mia prima app con Vite!'
+        }
+  
+      },
+  
+    }
+  
+    
+  
+  </script>
+  
+  <style>
+  
+  </style>
+  
